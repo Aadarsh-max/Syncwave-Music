6765ed4092563ad57e6409588649cd13f40b2d95
Adarsh Shrivastav <aadarshshrivastav01@gmail.com>
2026-07-08 19:32:50 +0530

    added hooks

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@ import { Outlet, useLocation } from 'react-router-dom'
 import Sidebar from './components/layout/Sidebar.jsx'
 import Topbar from './components/layout/Topbar.jsx'
 import PlayerBar from './components/player/PlayerBar.jsx'
+import { ToastContainer } from './components/ui/Toast.jsx'
+import useToast from './hooks/useToast.js'
 
 export default function App() {
   const location = useLocation()
+  const { toasts, removeToast } = useToast()
 
   if (location.pathname === '/login') return <Outlet />
 
   return (
-    <div className="flex h-screen w-screen bg-[#0a0a0a]">
+    <div className="flex h-screen w-screen overflow-hidden bg-[#06060f] text-white" style={{ fontFamily: 'system-ui, -apple-system, sans-serif' }}>
       <Sidebar />
-      <div className="flex flex-col min-w-0 flex-1 ml-[240px]">
+      <div className="flex flex-col flex-1 overflow-hidden ml-[240px]">
         <Topbar />
         <main className="flex-1 overflow-y-auto pb-[90px]">
           <Outlet />
         </main>
       </div>
       <PlayerBar />
+      <ToastContainer toasts={toasts} removeToast={removeToast} />
     </div>
   )
 }
